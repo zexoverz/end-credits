f66a7b54f0c06b884b5fd522e5917cc9ec51769c
zexoverz <faisalfirdani01@gmail.com>
2026-09-26 21:27:36 +0900

    test: two new message codes

--- lib/messages.test.ts
+++ lib/messages.test.ts
@@ -17,7 +17,7 @@ describe("msg", () => {
   });
 
   it("has every code from DESIGN §11 plus the ones added during the build (decisions.md)", () => {
-    expect(Object.keys(MESSAGES)).toHaveLength(57);
+    expect(Object.keys(MESSAGES)).toHaveLength(59);
   });
 
   it("names the repo and package in SPOOF_REPO", () => {
